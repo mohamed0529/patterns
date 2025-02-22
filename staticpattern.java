e01b9a8428fb747fc5d5777e73dff2aad423a884
mohamed0529 <mohamedishaq781@gmail.com>
2025-02-22 19:57:59 +0530

    print stmt has been changed

--- staticpattern.java
+++ staticpattern.java
@@ -3,9 +3,10 @@ class staticpattern{
        int n=10;
        for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            System.out.println("*"+" ");
+            System.out.print("*"+" ");
             
         }
+        System.out.println();
            
        }
     }
