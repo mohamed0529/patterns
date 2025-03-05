793a2725c529bac0369b271cb304744336d430a8
mohamed0529 <mohamedishaq781@gmail.com>
2025-03-05 11:24:31 +0530

    numbers patterns

---
+++ Numpattern/numberpattern.java
@@ -0,0 +1,20 @@
+public class numberpattern {
+    
+        public static void main(String[] args) {
+            int n=5; 
+            for (int row = 0; row < n; row++) {
+                for (int col = 0; col <= row; col++) {
+                    System.out.print(col + " ");
+                    
+                }
+                System.out.println();
+    
+                
+            }
+            
+        }
+        
+    }
+    
+    
+
---
+++ Numpattern/numberpattern2.java
@@ -0,0 +1,20 @@
+public class numberpattern2 {
+  
+        public static void main(String[] args) {
+            int n=5; 
+            for (int row = 0; row < n; row++) {
+                for (int col = 0; col <= row; col++) {
+                    System.out.print(row +" ");
+                    
+                }
+                System.out.println();
+    
+                
+            }
+            
+        }
+        
+    }
+    
+    
+
