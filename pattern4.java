a523c4e67e81b62815b8540265fb75a25858287d
mohamed0529 <mohamedishaq781@gmail.com>
2025-03-05 11:30:37 +0530

    unwanted for loop has been removed

--- pattern4.java
+++ pattern4.java
@@ -2,10 +2,7 @@ public class pattern4{
     public static void main(String[] args) {
         int n=5;
         for (int row = n; row >=1; row--) {
-            for (int colspa = n; colspa <n; colspa--) {
-                System.out.print(" ");
-                
-            }
+           
             for (int star = 1  ; star <=row; star++) {
                 System.out.print("*"+ " ");
                 
